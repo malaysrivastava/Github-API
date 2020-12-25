211e455584e47f01ec727e0aeca8e16dd33ba07f
Malay Srivastava <57169971+malaysrivastava@users.noreply.github.com>
2020-12-25 16:42:31 +0530

    ready-for-production---storage-left

--- src/components/View.js
+++ src/components/View.js
@@ -3,35 +3,56 @@ import { ToastContainer, toast } from 'react-toastify';
 import '../styles/view.css'
 
 
-const View =(avatar)=> {
+const View =({avatar,local})=> {
+
+    const check =(local)=>{
+       if (local) {
+        return(
+           <button onClick={localStorage} className="btn btn-warning">Save</button>
+        )       
+     } else {
+       return null
+     } 
+    }
+
+    const localStorage =()=>{
+
+    }
+
     return(
-      <div className="text-light">
+      <>
+      <ToastContainer/>
+      <div className="avatar container text-center text-light">
+       <div className="row m-auto">
       {/* Avatar */}
       <h2>{avatar.message}</h2>
-      <div>
+      <div className="col-sm-3 dp">
         <img src={avatar.avatar_url} alt={avatar.name} />
       </div>
       {/* Info */}
-      <div className="container">
+      <div className="col-sm-6 text-left info">
         {/* Name */}
-        <div>Name: {avatar.name}</div>
+        <div className="name">Name: {avatar.name}</div>
         {/* Github Link */}
-        <div>
+        <div className="link">
           GitHub Link:{" "}
           <a href={avatar.html_url} target="_blank" rel="noopener noreferrer">
             {avatar.html_url}
           </a>
         </div>
         {/* email */}
-        <div>Email: {avatar.Email}</div>
+        <div className="mail">Email: {avatar.email}</div>
         {/* Location */}
-        <div>Location:{avatar.location}</div>
+        <div className="loc">Location:{avatar.location}</div>
         {/* Public repo */}
-        <div>Public repos: {avatar.public_repos}</div>
+        <div className="repo">Public repos: {avatar.public_repos}</div>
         {/* Info */}
-        <div>Bio: {avatar.bio}</div>
+        <div className="bio">Bio: {avatar.bio}</div>
+      </div>
+      {check(local)}
       </div>
     </div>
+    </>
     )
 }
 
--- src/core/Home.js
+++ src/core/Home.js
@@ -8,7 +8,40 @@ const Home = () => {
 
    const [user,setUser] = useState({Uname:''})
 
-   const [userdata,setUserdata] = useState([])
+   const [userdata,setUserdata] = useState({
+      login: "Nyamador",
+id: 48738520,
+node_id: "MDQ6VXNlcjQ4NzM4NTIw",
+avatar_url: "https://avatars3.githubusercontent.com/u/48738520?v=4",
+gravatar_id: "",
+url: "https://api.github.com/users/Nyamador",
+html_url: "https://github.com/Nyamador",
+followers_url: "https://api.github.com/users/Nyamador/followers",
+following_url: "https://api.github.com/users/Nyamador/following{/other_user}",
+gists_url: "https://api.github.com/users/Nyamador/gists{/gist_id}",
+starred_url: "https://api.github.com/users/Nyamador/starred{/owner}{/repo}",
+subscriptions_url: "https://api.github.com/users/Nyamador/subscriptions",
+organizations_url: "https://api.github.com/users/Nyamador/orgs",
+repos_url: "https://api.github.com/users/Nyamador/repos",
+events_url: "https://api.github.com/users/Nyamador/events{/privacy}",
+received_events_url: "https://api.github.com/users/Nyamador/received_events",
+type: "User",
+site_admin: false,
+name: "Desmond ",
+company: "@Velocity-Corp",
+blog: "Nyamador.me",
+location: "Ghana",
+email: null,
+hireable: null,
+bio: "FullStack Engineer | Pluralsight Author",
+twitter_username: "DesmondNyamador",
+public_repos: 52,
+public_gists: 8,
+followers: 27,
+following: 1,
+created_at: "2019-03-19T23:34:18Z",
+updated_at: "2020-07-29T11:45:10Z"
+   })
 
    const {Uname} = user
 
@@ -29,6 +62,7 @@ const Home = () => {
                    console.log(data)
                    setUserdata(data)
                    console.log(userdata)
+                   console.log(userdata.id)
 
                })     
                .catch((error) => {
@@ -36,17 +70,17 @@ const Home = () => {
                  console.log("Oops! We have an error", error);
                });
 
-           },[user.Uname])
+           },[Uname])
    
   
      return(
         <>
         <ToastContainer/>
          <Bar handleChange={handleChange}/>
-         <div className="display">
+         <div className="display container">
          {userdata.id  ? (
             
-            <UserView profile={userdata}/>
+            <UserView avatar={userdata} local={true}/>
             
             
           ) : (
